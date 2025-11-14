eee2e187e31500266a109ba47be85119573c4daf
Matheus <matheus.torres@cnj.jus.br>
2025-11-14 20:49:31 -0300

    Adicionado o suporte SSL para Aiven

--- config/database.js
+++ config/database.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Criar conexão com o banco de dados
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// Conectar ao banco
 connection.connect((err) => {
   if (err) {
     console.error('❌ Erro ao conectar ao banco de dados:', err.message);
